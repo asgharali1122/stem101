a9335246eb6c6f12a967c9a5e13af9ca7124a22e
Asghar Ali <syed.aaaasghar123@gmail.com>
2022-04-22 17:03:24 +0500

    delete

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import changeNumber from './updaown'
-import dataHandle from './dataHandle'
-
-const rootReducers = combineReducers ({
-    changeNumber,
-    dataHandle,
-})
-export default rootReducers;
